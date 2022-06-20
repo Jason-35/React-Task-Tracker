0b06f8ba0c7e8fc9a82eae24c3e3fceb898aa931
Jason Wu <jsonwu34@gmail.com>
2022-06-20 00:55:33 -0400

    finish task tracker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Birthday",
+      day: "June 15 2000",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Today",
+      day: "June 19 2022",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Work Day",
+      day: "June 15 2000",
+      reminder: false,
+    },
+  ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
+    <div className="container">
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAddTask={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task"
+      )}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAddTask }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAddTask ? "red" : "green"}
+        text={showAddTask ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+export default Header;
